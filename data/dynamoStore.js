36dbda832e2807bd26bb6dffe481d94c37f6c1ed
Lore Wymetal <lore@paycafe.com>
2022-12-13 11:23:44 -0800

    Update for DynamoDB

---
+++ data/dynamoStore.js
@@ -0,0 +1,57 @@
+const {
+  DynamoDBClient
+} = require('@aws-sdk/client-dynamodb')
+
+const {
+  DynamoDBDocumentClient,
+  GetCommand,
+  PutCommand,
+  ScanCommand
+} = require('@aws-sdk/lib-dynamodb')
+
+function getClient() {
+  const marshallOptions = {
+    convertClassInstanceToMap: true
+  }
+  const client = new DynamoDBClient({ region: 'us-west-1' }) // check this
+  return DynamoDBDocumentClient.from(client, { marshallOptions })
+}
+
+async function putItem(table, item) {
+  const params = {
+    TableName: table,
+    Item: item
+  }
+  const client = getClient()
+  const command = new PutCommand(params)
+  return client.send(command)
+}
+
+async function getAllItems(table) {
+  const params = {
+    TableName: table
+  }
+  const client = getClient()
+  const command = new ScanCommand(params)  // only 1MB at a time
+  const response = await client.send(command)
+  return response.Items
+}
+
+async function getItem(table, idKey, id) {
+  const params = {
+    TableName: table,
+    Key: {
+      [idKey]: id
+    }
+  }
+  const client = getClient()
+  const command = new GetCommand(params)
+  const response = await client.send(command)
+  return response.Item
+}
+
+module.exports = {
+  getAllItems,
+  getItem,
+  putItem
+}
